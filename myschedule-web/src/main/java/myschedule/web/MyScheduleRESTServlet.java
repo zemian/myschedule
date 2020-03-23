00bc545a94bc9b07191d4d2536224c4f164d928a
Zemian Deng <zemian.deng@oracle.com>
2020-03-22 21:56:25 -0400

    Improve servlet path processing

--- myschedule-web/src/main/java/myschedule/web/MyScheduleRESTServlet.java
+++ myschedule-web/src/main/java/myschedule/web/MyScheduleRESTServlet.java
@@ -6,11 +6,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import myschedule.quartz.extra.SchedulerTemplate;
 import org.quartz.JobDetail;
-import org.quartz.Scheduler;
 import org.quartz.Trigger;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,15 +18,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuartzRESTServlet extends HttpServlet {
+@WebServlet("/api")
+public class MyScheduleRESTServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String contextPath = req.getContextPath();
+        String appPath = contextPath + "/api";
         String uri = req.getRequestURI();
         List<?> list = null;
-        if (uri.startsWith("/myschedule/api/schedulers")) {
-            list = getSchedulers();
-        } else if (uri.startsWith("/myschedule/api/jobs")) {
-            String name = uri.substring("/myschedule/api/jobs/".length());
+        if (uri.startsWith(appPath + "/scheduler-settings")) {
+            list = getSchedulerSettings();
+        } else if (uri.startsWith(appPath + "/jobs")) {
+            String name = uri.substring((appPath + "/jobs/").length());
             list = getJobs(name);
         }
 
@@ -41,7 +44,7 @@ public class QuartzRESTServlet extends HttpServlet {
         out.print(output);
     }
 
-    private List<SchedulerSettings> getSchedulers() {
+    private List<SchedulerSettings> getSchedulerSettings() {
         MySchedule mySchedule = MySchedule.getInstance();
         List<String> names = mySchedule.getSchedulerSettingsNames();
         List<SchedulerSettings> result = new ArrayList<>();
